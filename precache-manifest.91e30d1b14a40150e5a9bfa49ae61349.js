535694aff87934b5aec2944b53a27f18744e781b
gselfzero <gselfzero@users.noreply.github.com>
2019-12-21 01:38:14 +0300

    Updates

---
+++ precache-manifest.91e30d1b14a40150e5a9bfa49ae61349.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2542d0827df585dd6362",
+    "url": "/mbeta/static/css/main.fcffc1c6.chunk.css"
+  },
+  {
+    "revision": "2542d0827df585dd6362",
+    "url": "/mbeta/static/js/main.5f94a593.chunk.js"
+  },
+  {
+    "revision": "2ed9fe5d87a767960899",
+    "url": "/mbeta/static/js/runtime~main.84ee916d.js"
+  },
+  {
+    "revision": "13d8484a9127da9c8eb5",
+    "url": "/mbeta/static/css/2.37304390.chunk.css"
+  },
+  {
+    "revision": "13d8484a9127da9c8eb5",
+    "url": "/mbeta/static/js/2.137622d8.chunk.js"
+  },
+  {
+    "revision": "422a1cffb3592ad79374c925b6282f0c",
+    "url": "/mbeta/static/media/click.422a1cff.png"
+  },
+  {
+    "revision": "0569ebf098f148e70ffc633d30b156d1",
+    "url": "/mbeta/static/media/api.0569ebf0.php"
+  },
+  {
+    "revision": "0399b6c2723168781a07941c077efbaa",
+    "url": "/mbeta/index.html"
+  }
+];
